c4d81951ce74701742f46360e3e18ab60eb912a3
Sarah Min <sarahmin@Sarahs-MacBook-Air.local>
2018-04-12 17:04:00 -0400

    math

--- main.js
+++ main.js
@@ -3,27 +3,32 @@ $(document).ready(function () {
 
 
 
-			// $("input").on("keyup keydown keypress change", function (e) {
-			// 	var income = parseFloat($(".income").val());
-			// "var savings = income - (rent + expenses + loans);"
+	// $("input").on("keyup keydown keypress change", function (e) {
+	// 	var income = parseFloat($(".income").val());
+	// "var savings = income - (rent + expenses + loans);"
 
-			$("input").on("keyup keydown keypress change", function (e) {
-				var income = parseFloat($(".income").val());
-				var years = parseFloat($(".years").val());
-				var savings = parseFloat($(".savings").val());
+	$("input").on("keyup keydown keypress change", function (e) {
+		var income = parseFloat($(".income").val());
+		var years = parseFloat($(".years").val());
+		var savings = parseFloat($(".savings").val());
+		var rent = parseFloat($(".rent").val());
+		var loans = parseFloat($(".loans").val());
+		var expenses = parseFloat($(".expenses").val());
 
-				var brooklyn = 226000,
-					var bronx = 78875,
-						var queens = 187000,
-							var manhattan = 500000;
 
-				$("select").on("change", function () {
-					var borough = parseFloat($(this).val());
-					var savings = income - (rent + expenses + loans);
-					var years = (borough / savings) * 12;
-					// How many years it will take the user to buy a house
-					$(".years").find("span").text(years);
+		var brooklyn = 226000,
+			bronx = 78875,
+			queens = 187000,
+			manhattan = 500000;
 
-				});
+		$("select").on("change", function () {
+			var borough = parseFloat($(this).val());
+			var savings = income - (rent + expenses + loans);
+			var years = borough / (savings * 12);
+			// How many years it will take the user to buy a house
+			$(".years").find("span").text(years);
 
-			});
+		});
+
+	});
+});
